ed37d7b6168ceb885a37dc18230e53d072397805
Rana <rana.binmadi@gmail.com>
2025-10-16 11:55:40 +0300

    Initial Commit

---
+++ plantsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  plantsApp
+//
+//  Created by Rana on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ plantsApp/plantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsAppApp.swift
+//  plantsApp
+//
+//  Created by Rana on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
